6e29e7ecc4097c7832b73ec7d4947f12224c1631
kumar-kunal <kunal.sawarn@gmail.com>
2020-01-22 01:06:16 +0530

    first commit

---
+++ background.js
@@ -0,0 +1,1 @@
+total_ads=0,flag=0,prev=0,next=0,chrome.runtime.onMessage.addListener((t,a)=>{0==flag?(prev=t.data,flag=1):(next=t.data,flag=0),prev!=next&&(total_ads+=t.data),show_count="",total_ads>100?(show_count="100+",total_ads=101):show_count=""+total_ads,"content"===t.from&&"show_ads_count"===t.subject&&chrome.browserAction.setBadgeText({text:show_count},function(t){})});
