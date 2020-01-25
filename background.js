186faad66d9035b89e08ba2e41e42de5f42d49f6
kumar-kunal <kunal.sawarn@gmail.com>
2020-01-25 19:05:08 +0530

    added code for blocking ad sever

--- background.js
+++ background.js
@@ -1,1 +1,17 @@
-total_ads=0,flag=0,prev=0,next=0,chrome.runtime.onMessage.addListener((t,a)=>{0==flag?(prev=t.data,flag=1):(next=t.data,flag=0),prev!=next&&(total_ads+=t.data),show_count="",total_ads>100?(show_count="100+",total_ads=101):show_count=""+total_ads,"content"===t.from&&"show_ads_count"===t.subject&&chrome.browserAction.setBadgeText({text:show_count},function(t){})});
+total_ads = 0, flag = 0, prev = 0, next = 0, chrome.runtime.onMessage.addListener((t, a) => {
+    0 == flag ? (prev = t.data, flag = 1) : (next = t.data, flag = 0), prev != next && (total_ads += t.data), show_count = "", total_ads > 100 ? (show_count = "100+", total_ads = 101) : show_count = "" + total_ads, "content" === t.from && "show_ads_count" === t.subject && chrome.browserAction.setBadgeText({
+        text: show_count
+    }, function(t) {})
+});
+
+var enabled = true;
+chrome.webRequest.onBeforeRequest.addListener(
+	function(details) {
+		if(enabled) {
+			console.log("blocking:", details.url);
+		}
+		return {cancel: enabled };
+	},
+	{urls: blocked_domains},
+	["blocking"]
+);
--- content.js
+++ content.js
@@ -1,1 +1,17 @@
-function ad_blocker(){if(ads_queries=document.querySelectorAll('[id^="google"]'),ads_queries_class=document.querySelectorAll('[class^="Google"]'),counter=0,ads_queries.length>0)for(ad of ads_queries)counter++,ad.style.display="none";if(ads_queries_class.length>0)for(ad of ads_queries_class)counter++,ad.style.display="none";return counter}ads_count=0,setInterval(function(){ads_count=ad_blocker();try{chrome.runtime.sendMessage({from:"content",subject:"show_ads_count",data:ads_count})}catch(e){}},1e3);
+function ad_blocker() {
+    if (ads_queries = document.querySelectorAll('[id^="google"]'), ads_queries_class = document.querySelectorAll('[class^="Google"]'), counter = 0, ads_queries.length > 0)
+        for (ad of ads_queries) counter++, ad.style.display = "none";
+    if (ads_queries_class.length > 0)
+        for (ad of ads_queries_class) counter++, ad.style.display = "none";
+    return counter
+}
+ads_count = 0, setInterval(function() {
+    ads_count = ad_blocker();
+    try {
+        chrome.runtime.sendMessage({
+            from: "content",
+            subject: "show_ads_count",
+            data: ads_count
+        })
+    } catch (e) {}
+}, 1e3);
